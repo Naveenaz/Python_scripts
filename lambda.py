0ffe59455997522c25a72e8e675b7ddc98bbab09
Naveen Zunjarwad <jobfornaveeninuk@gmail.com>
2013-09-03 16:13:55 +0100

    Sample lambda and its equivalent function in Python

---
+++ lambda.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+answer=lambda x,y: x*y
+
+print "Lambda :", (answer(5,6))
+
+def multiply(x,y):
+	print "From function:", x*y
+
+multiply(5,6)
+
+
+
+
+-------------------------
+Output:
+
+python lambda.py
+
+Lambda : 30
+From function: 30
